6be6702cffce92db7ab086d20e913f570c995089
RandolphCarter0 <71259424+RandolphCarter0@users.noreply.github.com>
2024-02-29 20:33:17 +0800

    Add files via upload

---
+++ strategy.py
@@ -0,0 +1,51 @@
+
+
+
+def curriculum_strategy_gauss(iter_num,args):
+    if iter_num == 1600 or iter_num == 2400 or iter_num == 3200:  # vgg alexnet
+        args.prior_batch = args.prior_batch * 2
+    if iter_num % args.gauss_t0 == 0 and args.std < 127:
+        args.std = args.std + args.gauss_gamma
+    if args.std > 127:
+        args.std = 127
+    return args
+
+
+
+def curriculum_strategy_jigsaw(iter_num,args):
+    if iter_num == 1600 or iter_num==3200:
+        args.prior_batch = args.prior_batch*2
+    if iter_num % args.jigsaw_t0 == 0 and iter_num < args.jigsaw_end_iter:
+        args.fre = args.fre + args.jigsaw_gamma
+
+    return args
+
+def curriculum_strategy_jigsaw_resnet152(iter_num,args):
+    if iter_num == 1600 or iter_num==3200:
+        args.prior_batch = args.prior_batch*2
+
+    if iter_num < 1000:
+        if iter_num % 200 == 0:
+            args.fre += 1
+
+    if iter_num >= 1000:  # and iter_num <4200:
+        if iter_num % 400 == 0:
+            args.fre += 1
+
+    return args
+
+
+def curriculum_strategy_jigsaw_googlenet(iter_num,args):
+    if iter_num == 1600 or iter_num == 2400 or iter_num == 3200:
+        args.prior_batch = args.prior_batch * 2
+    if iter_num % 400 == 0:
+        args.fre += 1
+
+
+    if iter_num > 1200 and iter_num <= 3000:
+
+        if iter_num % 600 == 0:
+            args.fre += 1
+
+    return args
+
